6d729d1f0fc3b9987c5e8df4b1651a36b5318f04
EuclidesBarrozo <euclidesbarrozo@hotmail.com>
2024-02-03 16:24:30 -0300

    all1
    subindo tudo pela primeira vez

---
+++ js/adaptacaoAutomatica.js
@@ -0,0 +1,9 @@
+const menu1 = document.getElementById("menu1");
+const menu1_1 = document.getElementById("menu1_1");
+const menu1_2 = document.getElementById("menu1_2");
+
+function adaptarMenu(){
+    menu1.innerHTML     = "Cristianismo";
+    menu1_1.innerHTML   = "Bíblia";
+    menu1_2.innerHTML   = "Literatura Cristã";
+}
